abb413fd77b620a3cc46ebe57321857e7b727d15
ldumay <dumay.famille@gmail.com>
2023-02-22 13:13:14 +0100

    Update tp1_prog2.c

--- tp1_prog2.c
+++ tp1_prog2.c
@@ -2,21 +2,22 @@
 #include <omp.h>
 
 //Nombre de steps
+float sum;
 int x = 100;
 
 int main()
 {
+    printf("steps : %d\n", x);
+
     #pragma omp parallel
     {
-        float sum;
         float f;
-
         for(int i=0; i<x; i++){
             f = 4.0 / ( 1 + i ^ 2);
             sum += f;
         }
         printf("f: %f\n", f);
-
-        printf("sum=%f", sum);
     }
+
+    printf("Resultat : sum=%f\n", sum);
 }
