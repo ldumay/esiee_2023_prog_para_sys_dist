f68b6f4e5bb750fb0f104ce2838b80eccf806129
ldumay <dumay.famille@gmail.com>
2023-02-22 14:33:18 +0100

    Update tp1_prog2.c

--- tp1_prog2.c
+++ tp1_prog2.c
@@ -3,28 +3,28 @@
 #include <omp.h>
 
 //Nombre de steps
-float sum = 0;
+float result = 0;
 int x = 100;
 
 float fonction_de_f(int x){
-    return 4.0 / ( 1 + x ^ 2);
+    return 4.0 / ( 1 + x * 2);
 }
 
-float add_to_sum_des_f(float x){
-    sum += x;
+float sum_des_f(){
+    float sum = 0;
+    for(int i=0; i<x; i++){
+        float f = fonction_de_f(i);
+        printf("[f:%f]", f);
+        sum += f;
+    }
+    return sum;
 }
 
 int main()
 {
     printf("steps : %d\n", x);
-    printf("sum : %f\n", sum);
-
-    float f;
-    for(int i=0; i<x; i++){
-        f = fonction_de_f(i);
-        printf("f: %f\n", f);
-        add_to_sum_des_f(f);
-    }
-
-    printf("Resultat : sum=%f\n", sum);
+    result = sum_des_f();
+    printf("sum : %f\n", result);
+    sum_des_f();
+    printf("Resultat : sum=%f\n", result);
 }
