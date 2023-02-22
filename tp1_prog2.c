80c34166a878d1e06fe2e28b78b6dca2b32cb1ba
ldumay <dumay.famille@gmail.com>
2023-02-22 13:06:40 +0100

    Update tp1_prog2.c

--- tp1_prog2.c
+++ tp1_prog2.c
@@ -1,16 +1,15 @@
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/types.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <omp.h>
 
 //Nombre de steps
 int x = 100;
 
-#pragma omp parallel
+int main()
 {
-    for(int i=0; i<x; i++){
-        4.0 / ( 1 + i ^ 2)
+    #pragma omp parallel
+    {
+        for(int i=0; i<x; i++){
+            4.0 / ( 1 + i ^ 2);
+        }
     }
 }
