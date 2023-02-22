52042dcb722f753278b7fcbc5d7b309f1a911397
ldumay <dumay.famille@gmail.com>
2023-02-22 15:13:09 +0100

    Update tp1_prog2.c

--- tp1_prog2.c
+++ tp1_prog2.c
@@ -2,25 +2,27 @@
 #include <math.h>
 #include <omp.h>
 
-int x = 100;
-float moyenne, f, sum, step = 0.0;
+int num_steps = 100;
+float x, moyenne, f, sum, step = 0.0;
 
 int main()
 {
-    printf("steps : %d\n", x);
-    step = 1.0;
+    printf("steps : %d\n", num_steps);
+    //
+    step = 1.0 / (double) num_steps;
     //Somme des fonctions f
-    for(int i=0; i<x; i++){
+    for(int i=0; i<num_steps; i++){
+        //déplacement d'intervale
         x = (i+0.5) * step;
         //fonction f
         f = 4.0 / ( 1.0 + x * x);
-        //-
+        //Somme du périmètre
         printf("[f:%f]", f);
         sum += f;
     }
     //-
     printf("\nsum : %f\n", sum);
     //-
-    moyenne = sum / x;
+    moyenne = sum * step;
     printf("\nResultat : moyenne=%f\n", moyenne);
 }
