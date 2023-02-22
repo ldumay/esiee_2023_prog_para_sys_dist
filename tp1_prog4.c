18ee0126a1ea1e2283202cac6d0d26ae93211532
ldumay <dumay.famille@gmail.com>
2023-02-22 15:21:28 +0100

    Update tp1_prog4.c

--- tp1_prog4.c
+++ tp1_prog4.c
@@ -2,14 +2,27 @@
 #include <math.h>
 #include <omp.h>
 
-static long num_steps = 10000;
-double step;
-int main(){
-    int i; double x, pi, sum = 0.0;
+float getPI(){
+    long num_steps = 10000;
+    int i; double step, x, pi, sum = 0.0;
     step = 1.0 / (double) num_steps;
     for(i=0; i<num_steps; i++){
         x = (i+0.5) * step;
         sum = sum + 4.0 / (1.0 + x * x);
     }
-    pi = step * sum;
+    return pi = step * sum;
+}
+
+int main()
+{
+	#pragma omp parallel
+	{
+		int NB_THREADS = omp_get_num_threads();
+        int ID = omp_get_thread_num();
+        int TIME = omp_get_wtime();
+        printf("Threads");
+		printf("\n[Nb threads:(%d)]", NB_THREADS);
+        printf("[Id thread:(%d)]", ID);
+        printf("[Temps en secondes:(%d)]", TIME);
+	}
 }
