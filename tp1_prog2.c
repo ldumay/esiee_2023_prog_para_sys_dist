62b535e39c1d2dd9bbed2b3b7aa0fc5cee762e16
ldumay <dumay.famille@gmail.com>
2023-02-22 15:07:45 +0100

    Update tp1_prog2.c

--- tp1_prog2.c
+++ tp1_prog2.c
@@ -3,15 +3,17 @@
 #include <omp.h>
 
 int x = 100;
-float moyenne, f, sum = 0.0;
+float moyenne, f, sum, step = 0.0;
 
 int main()
 {
     printf("steps : %d\n", x);
+    step = 1.0;
     //Somme des fonctions f
     for(int i=0; i<x; i++){
+        x = (i+0.5) * step;
         //fonction f
-        f = 4.0 / ( 1 + i * i);
+        f = 4.0 / ( 1.0 + x * x);
         //-
         printf("[f:%f]", f);
         sum += f;
