d8f8b32f4d14cf846e1d43bb944ca74b031ab6a6
ldumay <dumay.famille@gmail.com>
2023-02-23 10:25:12 +0100

    Update tp1_prog5.c

--- tp1_prog5.c
+++ tp1_prog5.c
@@ -7,12 +7,13 @@ static long num_steps = 100000;
 double step;
 
 void main(){
-    int i, nthreads; double pi, sum[NUM_THREADS];
+    int i, nthreads; 
+    double pi, sum[NUM_THREADS];
     step = 1.0 / (double) num_steps;
     omp_set_num_threads(NUM_THREADS);
     #pragma omp parallel
     {
-        int i, id, nthrds;
+        int id, nthrds;
         double  x;
         id = omp_get_thread_num();
         nthrds = omp_get_num_threads();
